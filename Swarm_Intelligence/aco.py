84ea9710317f7352bbb20199f15cd1929d9c4d5c
Otniel Aguilar <otnieel.aguilar@gmail.com>
2014-05-08 03:33:04 -0500

    ACO terminado

--- Swarm_Intelligence/aco.py
+++ Swarm_Intelligence/aco.py
@@ -6,7 +6,7 @@ from math import e
 
 class AntColonyOptimization:
 	
-    def __init__(self, matrix_file):
+    def __init__(self, matrix_file, ants):
         text_matrix = self.read_info_from_file(matrix_file) 
         self.heuristic_matrix = self.parse_matrix(text_matrix)
         self.total_nodes = len(self.heuristic_matrix)
@@ -15,6 +15,9 @@ class AntColonyOptimization:
         # Factor de decremento de la feromona p (letra griega rho)
         self.P = 0.3
         self.Q = 1.0
+        self.used_arist_log = [[int('0') for j in range(self.total_nodes)]
+                               for i in range(self.total_nodes)]
+        self.ants = ants
 
     def init_pheromon_matrix(self, value = 1):
         """ Se inicia la matriz con un valor dado, por default 1 """
@@ -45,12 +48,19 @@ class AntColonyOptimization:
             matrix.append([int(item) for item in line[0].split(',')])
         return matrix
 
+    def total_cost_tour(self, tour):
+        arists = self.arists_from_tour(tour)
+        return sum([self.heuristic_matrix[i][j] for i,j in arists])
+
     def heuristic_info(self, n):
         """ Método para regresar un valor del peso coherente: mayor peso, mejor solución"""
         # Si n es 0, entonces la información heurística del arco Nij es de un mismo nodo
         if n == 0:
             return 0
         return 1.0 / n
+
+    def arists_from_tour(self, arist_list):
+        return [tuple(arist_list[n-2:n]) for n in range(2, len(arist_list)+1)]
 
     def heuristic_info_from_list(self, list_info):
         return [self.heuristic_info(n) for n in list_info]
@@ -82,70 +92,64 @@ class AntColonyOptimization:
                 list_prob[ant, adyacent] = self.arist_selecc_probability(ant, adyacent)
         return list_prob
         
-    def delta_tau(self, ant_tour):
-        """ """
-        return self.Q / len(ant_tour) 
-    
-    def update_pheromon(self):
-        pass
+    def update_pheromon(self, tour):
+        arists = self.arists_from_tour(tour)
+        self.update_arist_log(arists)
+        for i, j in arists:
+            sum_pheromon = sum([self.pheromon_laid() for k in range(self.heuristic_matrix[i][j])])
+            self.pheromon_matrix[i][j] = (1 - self.P) * self.pheromon_matrix[i][j] + sum_pheromon
+
+    def pheromon_laid(self):
+        return self.Q / self.total_nodes
+
+    def update_arist_log(self, arists):
+        for i,j in arists:
+            self.used_arist_log[i][j] += 1 
+
+    def best_tour(self, tours):
+        tour_cost = {self.total_cost_tour(tour):tour for tour in tours}
+        best = min(tour_cost.keys())
+        return tour_cost[best], best
 
     def build_tour(self):        
         tour = []
         V = [node for node in range(self.total_nodes)]
         first = 0
         current = 0
-        print 'V ', V
+        _sum = 0
         for node in range(self.total_arists):
             # Si el tour está vacío, se selecciona cualquier nodo para empezar. 
             if not tour:
                 current = random.choice(V)
                 first = current
-                tour.append(first)
-                print 'tour con primer nodo', tour
-            print 'current: ', current
-            print 'V antes de llamar a adyacent nodes: ', V
+                tour.append(first)    
             adyacent_nodes = self.adyacent_nodes(current, V)
-            print 'adyacent nodes: ', adyacent_nodes
             V.remove(current)
-            print 'V Después de remove ',current, ': ', V
             list_prob = self.list_selection_probability(adyacent_nodes, current)
-            myprob = random.random()
-            print 'list_prob!', list_prob
-            _sum = 0
-            print 'numero aleatorio ', myprob
+            random_probability = random.random()
             for nodes, probability in list_prob.items():
                 _sum += probability
-                print 'sumatoria probabilidad ', _sum
-                if _sum >= myprob or (myprob - _sum) / myprob < 1e-6:
+                if _sum >= random_probability or (random_probability - _sum) / random_probability < 1e-6:
                     current = nodes[1]
                     tour.append(current)
-                    print 'tour después de agregar ', current, ': ', tour
                     break
-            print
+        tour.append(first)
         return tour
-        
-if len(sys.argv) != 2:
-    print "Uso -> aco.py ARCHIVO_INSTANCIA"
+
+    def main(self):
+        total_tours = []
+        for ant in range(self.ants):
+            tour = self.build_tour()
+            total_tours.append(tour)
+            self.update_pheromon(tour)
+        for ant, tour in enumerate(total_tours):
+            print 'Ant ', ant, ' -> ', tour, 'costo -> ', self.total_cost_tour(tour)
+        best = self.best_tour(total_tours)
+        print 'best tour founded: ', best[0], 'cost: ', best[1]
+
+if len(sys.argv) != 3:
+    print "Uso -> aco.py ARCHIVO_INSTANCIA TOTAL_HORMIGAS"
     exit()
 
-aco = AntColonyOptimization(sys.argv[1])
-
-#print 'Matriz feromona\tMatriz heurística(costo de aristas)'
-#for index, i in enumerate(aco.pheromon_matrix):
-#    print i, '\t',  aco.heuristic_matrix[index], aco.heuristic_info_from_list(aco.heuristic_matrix[index]) 
-    
-#print aco.build_tour()
-
-#list_prob = aco.list_selection_probability(1)
-#myprob = random.random()
-
-# print 'my prob ', myprob
-# _sum = 0
-# for nodes, probability in list_prob.items():
-#     _sum += probability 
-#     #if _sum >= myprob:
-#     if _sum >= myprob or (myprob - _sum) / myprob < 1e-6:
-#         print nodes, probability
-#         break
-    
-print aco.build_tour()
+aco = AntColonyOptimization(sys.argv[1], int(sys.argv[2]))
+aco.main()
